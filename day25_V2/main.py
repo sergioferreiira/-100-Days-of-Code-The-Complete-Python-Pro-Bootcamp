ed520dc0f1b5473ded8cad61cb3603ede2b27f80
Sergio Ferreira <dev.sergioferreira@gmail.com>
2024-11-22 07:52:00 -0300

    states

---
+++ day25_V2/main.py
@@ -0,0 +1,25 @@
+import turtle , pandas
+
+states = pandas.read_csv("day25_V2/50_states.csv")
+
+while True:
+ stateName = input(str("type the name of the state: "))
+ print(stateName in states["state"])
+
+
+
+# print(states)
+
+# screen = turtle.Screen()
+# screen.title("U.S STATES")
+# image = "day25_V2/blank_states_img.gif"
+# screen.addshape(image)
+# turtle.shape(image)
+
+
+
+
+
+
+
+# screen.exitonclick()

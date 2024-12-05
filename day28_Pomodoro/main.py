7ac0fc14eb7b31294d8e7861589da7a1f5ea5a0d
Sergio Ferreira <dev.sergioferreira@gmail.com>
2024-12-05 08:20:31 -0300

    loop between timer and pause fixed

--- day28_Pomodoro/main.py
+++ day28_Pomodoro/main.py
@@ -1,35 +1,85 @@
 from tkinter import *
 import time
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 1
+SHORT_BREAK_MIN = 1
+LONG_BREAK_MIN = 2
+pauseCounter = 1
 
-minute = 25
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-second = 5
+def startCountDown():
+   global pauseCounter
+   if pauseCounter == 1 or pauseCounter == 3 or pauseCounter == 5 or pauseCounter == 7:
+      countDown(WORK_MIN * 60)
+      print(f"timer {pauseCounter}")
+   elif pauseCounter == 2 or pauseCounter == 4 or pauseCounter == 6:
+      countDown(SHORT_BREAK_MIN * 60)
+      print(f"pause {pauseCounter}")
+   elif pauseCounter == 8:
+      countDown(LONG_BREAK_MIN * 60)
+      print(f"pause long{pauseCounter}")
+   else:
+      canvas.itemconfig(timer_text,text=f"FIM")
+      print(pauseCounter)
 
-while second != 0:
-   second -=1
-   time.sleep(1)
 
-
-
+# def startShortBreak():
+#    shortPauseCountDown(SHORT_BREAK_MIN * 60)
+   
+# def startLongBreak():
+#    longPauseCountDown(LONG_BREAK_MIN * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def countDown(count):
+   global pauseCounter
+
+   count_min = math.floor(count / 60)
+   count_sec = count % 60
+   canvas.itemconfig(timer_text,text=f"{count_min:02}:{count_sec:02}")
+
+   if count > 0:
+      window.after(1,countDown,count -1)
+   if count == 0:
+      pauseCounter += 1
+      startCountDown()
 
 
+# def shortPauseCountDown(count):
+#    moment.config(text="Pause")
+#    global pauseCounter
+#    count_min = math.floor(count / 60)
+#    count_sec = count % 60
 
-  
+#    canvas.itemconfig(timer_text,text=f"{count_min:02}:{count_sec:02}")
+#    if count > 0:
+#       window.after(5,shortPauseCountDown,count -1)
+#    if count == 0:
+#       if pauseCounter == 4:
+#          startLongBreak()
+#       else:
+#          pauseCounter +=1
+#          print(pauseCounter)
+#          startCountDown()
+
+# def longPauseCountDown(count):
+#    moment.config(text="Long Pause")
+#    global pauseCounter
+#    count_min = math.floor(count / 60)
+#    count_sec = count % 60
+
+#    canvas.itemconfig(timer_text,text=f"{count_min:02}:{count_sec:02}")
+#    if count > 0:
+#       window.after(5,longPauseCountDown,count -1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -48,10 +98,10 @@ canvas.create_image(100,112,image =tomatoImg)
 canvas.grid(row=1,column=1)
 
 
-canvas.create_text(100,130,text=f"{minute:02}:{second:02}",fill="white",font=(FONT_NAME,30,"bold"))
+timer_text = canvas.create_text(100,130,text=f"00:00",fill="white",font=(FONT_NAME,30,"bold"))
 
 
-startButton = Button(text="Start")
+startButton = Button(text="Start", command=startCountDown)
 startButton.grid(row=2, column=0)
 
 emoji = "✔️"
